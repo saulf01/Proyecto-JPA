d2f2a451dad9434335ac920f0f473eb60df86d7e
saulf01 <saulfloresccv01@gmail.com>
2023-02-20 19:20:50 -0600

    Proyecto JPA

---
+++ src/main/java/com/buenmaestro/project_jpa/App.java
@@ -0,0 +1,13 @@
+package com.buenmaestro.project_jpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
